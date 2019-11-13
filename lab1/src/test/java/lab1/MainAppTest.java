6cd0ef2a321c7bc8c02f744dfb1ff9bcbd4bc289
SymphonyOfTranquility <mysechko01@gmail.com>
2019-11-13 03:21:50 +0200

    Bug fixes and minor improvements.

---
+++ lab1/src/test/java/lab1/MainAppTest.java
@@ -0,0 +1,15 @@
+package lab1;
+
+import com.jme3.math.*;
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainAppTest {
+    @Test public void testCheckMainAppCreation() {
+        MainApp app = new MainApp();
+    }
+}
---
+++ lab1/src/test/java/lab1/MainTest.java
@@ -0,0 +1,15 @@
+package lab1;
+
+import com.jme3.math.*;
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test public void testCheckMainCreation() {
+        Main testMain = new Main();
+    }
+}
