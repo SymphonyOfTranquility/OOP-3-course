a518900e7d0e24c5ba9d7da559afd98f7870cc66
SymphonyOfTranquility <mysechko01@gmail.com>
2019-11-16 22:28:40 +0200

    added jupiter test class

---
+++ lab1/src/test/java/lab1/JupiterTest.java
@@ -0,0 +1,56 @@
+package lab1;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class JupiterTest {
+	private static MainApp app1;
+	
+	@BeforeClass
+	public static void initApp1()
+	{
+		
+		app1 = new MainApp();
+        Runnable task = new Runnable() {
+            public void run() {
+            	app1.setShowSettings(false);
+            	app1.start();
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
+
+        while (!app1.isInit || !app1.jupiter.isInit)
+        {
+            Thread.yield();
+        }
+	}
+
+	@Test
+	public void createJupiter()
+	{
+		Jupiter jupiter = new Jupiter();
+		assertEquals("radius are equals",
+					  app1.getStateManager().getState(Jupiter.class).getMesh().getRadius(),
+				      jupiter.getMesh().getRadius(),
+				      0.0001);
+	
+	}
+	
+	@Test
+	public void checkJupiterWeight()
+	{
+		assertEquals("jupiter weight is 30000f",
+				app1.getStateManager().getState(Jupiter.class).getWeight(),
+				30000f,
+				0.0001);
+	}
+
+	
+	@AfterClass
+	public static void endApp1()
+	{
+		app1.stop(true); 
+	}
+
+}
--- lab1/src/test/java/lab1/VoyagerTest.java
+++ lab1/src/test/java/lab1/VoyagerTest.java
@@ -1,30 +1,11 @@
 package lab1;
 
-import com.jme3.app.SimpleApplication;
 import com.jme3.scene.Node;
 import com.jme3.asset.AssetManager;
-import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
-import com.jme3.material.RenderState.BlendMode;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.queue.RenderQueue.Bucket;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
-import com.jme3.scene.shape.Sphere;
-import com.jme3.texture.Texture;
-import com.jme3.asset.TextureKey;
-import com.jme3.math.Quaternion;
-import com.jme3.math.FastMath;
-import com.jme3.util.TangentBinormalGenerator;
-import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.renderer.Camera;
 import com.jme3.bullet.BulletAppState;
-import com.jme3.scene.Node;
-import com.jme3.math.*;
 import java.io.*;
 import java.util.Scanner;
-import java.lang.Math;
 
 import org.junit.*;
 import static org.junit.Assert.*;
